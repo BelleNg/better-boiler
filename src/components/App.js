502790a3edb48d17d69bd8f2a1e3dae7a0cf7929
Belle Nguyen <NgBelleK@gmail.com>
2020-08-02 11:40:42 -0500

    Create working get request from frontend

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      message: "not Hello World"
+      message: "not Hello World",
+      cats: ""
     };
     this.fetchCats = this.fetchCats.bind(this);
   }
@@ -17,9 +18,8 @@ export default class App extends Component {
   fetchCats() {
     fetch('http://localhost:3000/cats')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then((data) => this.setState(data))
   }
-
 
 
 
@@ -27,6 +27,7 @@ export default class App extends Component {
     return (
       <div>
         <p>{this.state.message}</p>
+        <p>{this.state.cats}</p>
       </div>
     );
   }
